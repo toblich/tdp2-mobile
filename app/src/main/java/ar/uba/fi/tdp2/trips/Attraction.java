afec8d132f0ad4323bedb515937cee1b100c713a
Tobi <tobias.lichtig@mulesoft.com>
2017-03-24 14:41:18 -0300

    Basic Attraction Cards
    Working cards with Title, Image and Description.
    The height is variable, according to the description length.
    TODO: Check what happens with long titles

---
+++ app/src/main/java/ar/uba/fi/tdp2/trips/Attraction.java
@@ -0,0 +1,15 @@
+package ar.uba.fi.tdp2.trips;
+
+import android.net.Uri;
+
+public class Attraction {
+    String name;
+    String description;
+    String photoUri;
+
+    public Attraction(String name, String description, String photoUri) {
+        this.name = name;
+        this.description = description;
+        this.photoUri = photoUri;
+    }
+}
--- app/src/main/java/ar/uba/fi/tdp2/trips/MainActivity.java
+++ app/src/main/java/ar/uba/fi/tdp2/trips/MainActivity.java
@@ -1,9 +1,18 @@
 package ar.uba.fi.tdp2.trips;
 
 import android.os.Bundle;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    // arnold's thumbs up
+    final String arnoldUrl = "http://3.bp.blogspot.com/-qUH2sD4GWB0/UUn5xBphLjI/AAAAAAAAA2o/MMYWv7n8sNw/s1600/thumb-up-terminator+pablo+M+R.jpg";
+    private List<Attraction> attractions;
 
     private String ubicacion;
 
@@ -18,6 +27,29 @@ public class MainActivity extends AppCompatActivity {
 
         this.setTitle(ubicacion);
 
+        Context context = this; // TODO verify this is right
+
+        initializeData();
+
+        RecyclerView rv = (RecyclerView) findViewById(R.id.rv);
+
+        LinearLayoutManager llm = new LinearLayoutManager(context);
+        rv.setLayoutManager(llm);
+
+        RV_AttractionAdapter adapter = new RV_AttractionAdapter(attractions, context);
+        rv.setAdapter(adapter);
+    }
+
+
+
+    // This method creates an ArrayList that has three Person objects
+    // Checkout the project associated with this tutorial on Github if
+    // you want to use the same images.
+    private void initializeData() {
+        attractions = new ArrayList<>();
+        attractions.add(new Attraction("Obelisco", "Una descripcion corta", arnoldUrl));
+        attractions.add(new Attraction("Usina del Arte", "Una descripción muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy muy larga.", arnoldUrl));
+        attractions.add(new Attraction("Casa Rosada", "Alguna descripción más...", arnoldUrl));
     }
 
     @Override
