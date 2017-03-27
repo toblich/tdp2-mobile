7eb17dc79ad06be37876f5b5a97dbf58d554ebaa
Santiago Fernandez <fernandezsantid@gmail.com>
2017-03-26 22:10:15 -0300

    Cambios en MainActivity (#10)
    * Se agrego un layout especial para cuando no se encuentran atracciones cerca del usuario y el manejo entre los 2 layouts de main.
    
    * Refactor en la clase MainActivity

--- app/src/main/java/ar/uba/fi/tdp2/trips/MainActivity.java
+++ app/src/main/java/ar/uba/fi/tdp2/trips/MainActivity.java
@@ -21,26 +21,24 @@ public class MainActivity extends AppCompatActivity {
     private String locality;
     private Double latitude;
     private Double longitude;
-
+    private boolean inMainWithoutAttractions = false;
     private Context localContext = this;
     private RecyclerView recyclerView;
+    private LinearLayoutManager llm;
+
+    //TODO: Debug para probar cambio de pantallas varias veces
+    //private boolean changeLayout = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         Bundle bundle = getIntent().getExtras();
         locality  = bundle.getString("locality");
         latitude  = bundle.getDouble("latitude");
         longitude = bundle.getDouble("longitude");
 
-        this.setTitle(locality);
-
-        recyclerView = (RecyclerView) findViewById(R.id.rv);
-
-        LinearLayoutManager llm = new LinearLayoutManager(localContext);
-        recyclerView.setLayoutManager(llm);
+        initializeActivity();
     }
 
     @Override
@@ -61,6 +59,8 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<List<Attraction>> call, Response<List<Attraction>> response) {
                 Log.d("TRIPS", "got attractions: " + response.body().toString());
                 attractions = response.body();
+
+                checkChangeLayout();
                 RV_AttractionAdapter adapter = new RV_AttractionAdapter(attractions, localContext);
                 recyclerView.setAdapter(adapter);
             }
@@ -72,6 +72,39 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("TRIPS", t.toString());
             }
         });
+    }
+
+    private void initializeActivity() {
+        setContentView(R.layout.activity_main);
+        this.setTitle(locality);
+        recyclerView = (RecyclerView) findViewById(R.id.rv);
+        llm = new LinearLayoutManager(localContext);
+        recyclerView.setLayoutManager(llm);
+    }
+
+    private void checkChangeLayout() {
+        //TODO: para probar que funciona lo del cambio cuando no hay atracciones y despues se agrega alguna.
+        //if (!changeLayout) {
+        //    attractions = new ArrayList<>();
+        //    changeLayout = true;
+        //}
+        //-------------------------------------------------------------------------------------------------
+        boolean isAttractionsEmpty = (attractions.size() == 0);
+
+        if (isAttractionsEmpty && !inMainWithoutAttractions) {
+            setContentView(R.layout.activity_main_without_attractions);
+            this.setTitle(locality);
+            inMainWithoutAttractions = true;
+            return;
+        }
+
+        if (!isAttractionsEmpty && inMainWithoutAttractions) {
+            initializeActivity();
+            inMainWithoutAttractions = false;
+            //TODO: Debug para probar cambio de pantallas cuando no hay atracciones
+            //changeLayout = false;
+            return;
+        }
     }
 
     @Override
