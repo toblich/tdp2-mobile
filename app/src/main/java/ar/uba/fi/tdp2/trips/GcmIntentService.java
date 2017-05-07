c5363a478d9b014534bb6ede317c6f2e861c6230
Francisco Landino <plandino@hotmail.com>
2017-05-06 21:26:51 -0300

    Handles push notifications

---
+++ app/src/main/java/ar/uba/fi/tdp2/trips/GcmIntentService.java
@@ -0,0 +1,63 @@
+package ar.uba.fi.tdp2.trips;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+public class GcmIntentService extends IntentService {
+
+    public GcmIntentService() {
+        super("GcmIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        String messageType = gcm.getMessageType(intent);
+
+        if (!extras.isEmpty() && GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+            sendNotification(extras);
+        }
+        GcmBroadcastReceiver.completeWakefulIntent(intent);
+    }
+
+    private void sendNotification(Bundle notification) {
+        NotificationManager mNotificationManager = (NotificationManager)
+                this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        String url = notification.getString("url");
+        String message = notification.getString("message");
+        String title = notification.getString("title");
+
+        // Opens the browser with the URL when the notification is taped
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        Uri notification_sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Ringtone ringtone = RingtoneManager.getRingtone(getApplicationContext(), notification_sound);
+        ringtone.play();
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.com_facebook_button_icon)
+                        .setContentTitle(title)
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
+                        .setContentText(message)
+                        .setAutoCancel(true);
+
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(0, mBuilder.build());
+    }
+}
